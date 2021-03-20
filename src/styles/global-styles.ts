fe4d7d7cb18aa01dabd49f39ee4ec61d37742155
Miguel Mendoza <miguelemrdev@gmail.com>
2021-03-20 14:40:22 -0400

    Update CSS for GlobalStyles

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -22,47 +22,70 @@ export const device = {
    desktopL: `(min-width: ${size.desktop})`,
 };
 
-export const GlobalStyles = createGlobalStyle`
+const stylesGlobals = css`
+   // Import FontFace
+   ${FontFamily}
 
-  // Import FontFace 
-  ${FontFamily}
+   html {
+      -ms-text-size-adjust: 100%;
+      -webkit-text-size-adjust: 100%;
+      font: 112.5%/1.45em 'Poppins', georgia, serif, sans-serif;
+      box-sizing: border-box;
+      overflow-y: auto;
+      height: 100vh;
+      background-color: #121212;
+   }
 
-  html {
-    -ms-text-size-adjust: 100%;
-    -webkit-text-size-adjust: 100%;
-    font: 112.5%/1.45em "Poppins", georgia, serif, sans-serif;
-    box-sizing: border-box;
-    overflow-y: auto;
-    background-color: #121212;
-  }
-
-  body {
-    margin: 0;
-    width: 100vw;
-    height: 100vh;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #fff;
-    font-family: "Poppins", georgia, serif;
-    font-weight: normal;
-    word-wrap: break-word;
-    font-kerning: normal;
-    -moz-font-feature-settings: "kern", "liga", "clig", "calt";
-    -ms-font-feature-settings: "kern", "liga", "clig", "calt";
-    -webkit-font-feature-settings: "kern", "liga", "clig", "calt";
-    font-feature-settings: "kern", "liga", "clig", "calt";
-    background-color: #121212;
-  }
-
-  #___gatsby{
-    height: 100%;
-    #gatsby-focus-wrapper{
+   body {
+      margin: 0;
+      width: 100%;
       height: 100%;
-    }
-  }
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      color: #fff;
+      font-family: 'Poppins', georgia, serif;
+      font-weight: normal;
+      word-wrap: break-word;
+      font-kerning: normal;
+      -moz-font-feature-settings: 'kern', 'liga', 'clig', 'calt';
+      -ms-font-feature-settings: 'kern', 'liga', 'clig', 'calt';
+      -webkit-font-feature-settings: 'kern', 'liga', 'clig', 'calt';
+      font-feature-settings: 'kern', 'liga', 'clig', 'calt';
+      background-color: #121212;
+   }
 
-  main{
-    height: 100%;
-    width: 100%;
-  }
+   #___gatsby {
+      height: 100%;
+      #gatsby-focus-wrapper {
+         height: 100%;
+      }
+   }
+
+   main {
+      height: 100%;
+      width: 100%;
+   }
+   /* 
+   *::-webkit-scrollbar,
+*::-webkit-scrollbar-thumb {
+  width: 26px;
+  border-radius: 13px;
+  background-clip: padding-box;
+  border: 10px solid transparent;
+}
+
+*::-webkit-scrollbar-thumb {        
+  box-shadow: inset 0 0 0 10px;
+} */
+
+   /* *::-webkit-scrollbar-button:start:decrement,
+   *::-webkit-scrollbar-button:end:increment {
+      height: 5px;
+      display: block;
+   } */
+
+   /* * {
+      scrollbar-color: #999 transparent;
+   } */
 `;
+export const GlobalStyles = createGlobalStyle`${stylesGlobals}`;
