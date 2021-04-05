285b1f3d8f13bce4e43936a46917eb7a5b253b6b
Miguel Mendoza <miguelemrdev@gmail.com>
2021-04-05 19:15:51 -0400

    Add console info for enviroment and version in build process

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ require('dotenv').config({
    path: `.env.${process.env.NODE_ENV}`,
 });
 
+console.info(`Enviroment: ${process.env.NODE_ENV} - ${version}`)
 module.exports = {
    siteMetadata: {
       title: `Zandome`,
@@ -65,5 +66,5 @@ module.exports = {
       // this (optional) plugin enables Progressive Web App + Offline functionality
       // To learn more, visit: https://gatsby.dev/offline
       // `gatsby-plugin-offline`,
-   ],
+   ]
 };
